2129ea859156abf83aeeb5eb20e6d96324ba16d0
Joonas Haapala <joonas@haapa.la>
2016-08-12 22:27:43 +0300

    fix indentation

--- src/main.rs
+++ src/main.rs
@@ -11,75 +11,75 @@ mod coder;
 
 fn encode(input: &str, output: &str) {
 
-	let f_in = File::open(input).expect("Unable to open file");
-	let f_out = File::create(output).expect("Unable to open file");
+    let f_in = File::open(input).expect("Unable to open file");
+    let f_out = File::create(output).expect("Unable to open file");
 
-	let mut f_in = BufReader::new(f_in);
-	let mut f_out = BufWriter::new(f_out);
+    let mut f_in = BufReader::new(f_in);
+    let mut f_out = BufWriter::new(f_out);
 
-	coder::encode(&mut f_in, &mut f_out).unwrap();
+    coder::encode(&mut f_in, &mut f_out).unwrap();
 }
 
 fn decode(input: &str, output: &str) {
 
-	let f_in = File::open(input).expect("Unable to open file");
-	let f_out = File::create(output).expect("Unable to open file");
+    let f_in = File::open(input).expect("Unable to open file");
+    let f_out = File::create(output).expect("Unable to open file");
 
-	let mut f_in = BufReader::new(f_in);
-	let mut f_out = BufWriter::new(f_out);
+    let mut f_in = BufReader::new(f_in);
+    let mut f_out = BufWriter::new(f_out);
 
-	coder::decode(&mut f_in, &mut f_out).unwrap();
+    coder::decode(&mut f_in, &mut f_out).unwrap();
 }
 
 fn print_usage(program: &str, opts: Options) {
-	let brief = format!("Usage: {} [options]", program);
-	print!("{}", opts.usage(&brief));
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let program = args[0].clone();
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-	let mut opts = Options::new();
-	opts.optopt("o", "", "set output file name", "NAME");
-	opts.optopt("m", "mode", "encode or decode", "MODE");
-	opts.optflag("h", "help", "print this help menu");
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optopt("m", "mode", "encode or decode", "MODE");
+    opts.optflag("h", "help", "print this help menu");
 
-	let matches = match opts.parse(&args[1..]) {
-		Ok(m) => { m }
-		Err(f) => { panic!(f.to_string()) }
-	};
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
 
-	if matches.opt_present("h") {
-		print_usage(&program, opts);
-		return;
-	}
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
 
-	let output = match matches.opt_str("o") {
-		Some(x) => x,
-		None => "out.bin".into()
-	};
+    let output = match matches.opt_str("o") {
+        Some(x) => x,
+        None => "out.bin".into()
+    };
 
-	let mode = match matches.opt_str("mode") {
-		Some(x) => x,
-		None => "encode".into()
-	};
-	if mode != "encode" && mode != "decode" {
-		print_usage(&program, opts);
-		return;
-	}
+    let mode = match matches.opt_str("mode") {
+        Some(x) => x,
+        None => "encode".into()
+    };
+    if mode != "encode" && mode != "decode" {
+        print_usage(&program, opts);
+        return;
+    }
 
-	let input = if !matches.free.is_empty() {
-		matches.free[0].clone()
-	} else {
-		print_usage(&program, opts);
-		return;
-	};
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
 
-	if mode == "encode" {
-		encode(&input, &output);
-	}
-	else {
-		decode(&input, &output);
-	}
+    if mode == "encode" {
+        encode(&input, &output);
+    }
+    else {
+        decode(&input, &output);
+    }
 }
--- src/sum_tree.rs
+++ src/sum_tree.rs
@@ -2,8 +2,7 @@ use num;
 use std::cmp;
 use num::NumCast;
 
-fn upper_power_of_two(v: u32) -> u32 {
-	let mut v = v;
+fn upper_power_of_two(mut v: u32) -> u32 {
     v -= 1;
     v |= v >> 1;
     v |= v >> 2;
@@ -15,104 +14,100 @@ fn upper_power_of_two(v: u32) -> u32 {
 }
 
 pub struct SumTree<T> {
-	tree: Vec<T>,
-	start_index: usize
+    tree: Vec<T>,
+    start_index: usize
 }
 
 impl<T> SumTree<T> where T: num::Num + Copy + num::traits::NumCast + cmp::Ord {
 
-	pub fn new(buckets: usize) -> SumTree<T> {
-		let start = (upper_power_of_two(buckets as u32)-1) as usize;
-		SumTree {
-			tree: vec![NumCast::from(0).unwrap(); (2*start+1) as usize],
-			start_index: start
-		}
-	}
+    pub fn new(buckets: usize) -> SumTree<T> {
+        let start = (upper_power_of_two(buckets as u32)-1) as usize;
+        SumTree {
+            tree: vec![NumCast::from(0).unwrap(); (2*start+1) as usize],
+            start_index: start
+        }
+    }
 
-	pub fn increment(&mut self, bucket: u32, amount: T) {
+    pub fn increment(&mut self, bucket: u32, amount: T) {
 
-		let mut index = bucket as usize + self.start_index;
+        let mut index = bucket as usize + self.start_index;
 
-		while index != 0 {
-			let k = self.tree[index];
-			self.tree[index] = k + amount;
-			index = (index - 1) >> 1;
-		}
-		let k = self.tree[0];
-		self.tree[0] = k + amount;
-	}
+        while index != 0 {
+            let k = self.tree[index];
+            self.tree[index] = k + amount;
+            index = (index - 1) >> 1;
+        }
+        let k = self.tree[0];
+        self.tree[0] = k + amount;
+    }
 
-	pub fn get(&self, bucket: usize) -> T {
-		//assert(index+start_index < m_tree.size());
-		self.tree[bucket + self.start_index]
-	}
+    pub fn get(&self, bucket: usize) -> T {
+        //assert(index+start_index < m_tree.size());
+        self.tree[bucket + self.start_index]
+    }
 
-	pub fn get_total(&self) -> T {
-		self.tree[0]
-	}
+    pub fn get_total(&self) -> T {
+        self.tree[0]
+    }
 
-	pub fn get_before(&self, bucket: usize) -> T {
-		let mut ret: T = NumCast::from(0).unwrap();
-		let mut index = bucket + self.start_index;
-		while index != 0 {
-			if (index & 1) == 0 {
-				ret = ret + self.tree[index-1];
-			}
-			index = (index-1) >> 1;
-		}
-		ret
-	}
+    pub fn get_before(&self, bucket: usize) -> T {
+        let mut ret: T = NumCast::from(0).unwrap();
+        let mut index = bucket + self.start_index;
+        while index != 0 {
+            if (index & 1) == 0 {
+                ret = ret + self.tree[index-1];
+            }
+            index = (index-1) >> 1;
+        }
+        ret
+    }
 
-	pub fn get_index(&self, value: T) -> usize {
+    pub fn get_index(&self, value: T) -> usize {
 
-		let mut index: usize = 0;
-		let mut value = value;
+        let mut index: usize = 0;
+        let mut value = value;
 
-		loop
-		{
-			let left = 2*index+1;
-			let right = 2*index+2;
+        loop
+        {
+            let left = 2*index+1;
+            let right = 2*index+2;
 
-			if left >= self.tree.len() {
-				break;
-			}
+            if left >= self.tree.len() {
+                break;
+            }
 
-			if value < self.tree[left] {
-				index = left;
-			}
-			else {
-				value = value - self.tree[left];
-				index = right;
-			}
-		}
-		index - self.start_index
-	}
+            if value < self.tree[left] {
+                index = left;
+            }
+            else {
+                value = value - self.tree[left];
+                index = right;
+            }
+        }
+        index - self.start_index
+    }
 }
 
 #[test]
 fn test_tree() {
 
-	let mut tree = SumTree::<u32>::new(8);
-	tree.increment(0,10);
-	tree.increment(1,10);
-	tree.increment(2,10);
-	tree.increment(7,1);
+    let mut tree = SumTree::<u32>::new(8);
+    tree.increment(0,10);
+    tree.increment(1,10);
+    tree.increment(2,10);
+    tree.increment(7,1);
 
-	assert!(tree.get_before(0) == 0);
-	assert!(tree.get_before(0) + tree.get(0) == 10);
-	assert!(tree.get_before(1) == 10);
-	assert!(tree.get_before(2) == 20);
-	assert!(tree.get_before(3) == 30);
-	assert!(tree.get_before(7) == 30);
+    assert!(tree.get_before(0) == 0);
+    assert!(tree.get_before(0) + tree.get(0) == 10);
+    assert!(tree.get_before(1) == 10);
+    assert!(tree.get_before(2) == 20);
+    assert!(tree.get_before(3) == 30);
+    assert!(tree.get_before(7) == 30);
 
-	assert!(tree.get_index(0) == 0);
-	assert!(tree.get_index(1) == 0);
-	assert!(tree.get_index(9) == 0);
-	assert!(tree.get_index(10) == 1);
-	assert!(tree.get_index(29) == 2);
-	assert!(tree.get_index(30) == 7);
+    assert!(tree.get_index(0) == 0);
+    assert!(tree.get_index(1) == 0);
+    assert!(tree.get_index(9) == 0);
+    assert!(tree.get_index(10) == 1);
+    assert!(tree.get_index(29) == 2);
+    assert!(tree.get_index(30) == 7);
 }
-
-
-
-
